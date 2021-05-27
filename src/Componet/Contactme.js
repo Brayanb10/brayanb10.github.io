6a96f21ac616a80f43fee2250c2b59d2b2e25f66
Brayan Batista <mcsmbatista@gmail.com>
2021-05-26 20:53:14 -0400

    modified Contactme.js

---
+++ src/Componet/Contactme.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Hey the best way to reach to me will be through my personal email, Dev.Batista20@gmail.com!
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linked in :
+                  {resumeData.linkedinId}
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
